5ced411b5e2abc65c534b841ea16acb8edd10df9
unknown <satya-1298>
2023-06-21 08:46:57 +0530

    [Satya]Added:Single Inheritence Program

---
+++ OOPS/Inheritance_Demo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    //Inheritance allow us to define a new class based on the exisisting class.
+    public class Inheritance_Demo //base class.
+    {
+        public string student = "Satya";
+
+        public void Parent()
+        {
+            Console.WriteLine("This is my base class");
+        }
+        
+    }
+    public class Child:Inheritance_Demo //Derived class
+    {
+        public string student_Name = "Satya";
+        public Child()
+        {
+            Console.WriteLine("This is my Derived class");
+        }
+        public void Name()
+        {
+            Console.WriteLine("Satya");
+        }
+
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.Parent();
+            child.Name();
+           Console.WriteLine( child.student_Name);
+            Console.WriteLine(child.student);
+
+            Console.ReadLine();
+        }
+    }
+}
